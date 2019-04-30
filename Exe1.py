78c933670ff74fc35cea5a7fdde1a7d8f1a8b0ee
racattaque <50136976+racattaque@users.noreply.github.com>
2019-04-30 11:53:04 +0200

    Update Exe1.py

--- Exe1.py
+++ Exe1.py
@@ -1,3 +1,5 @@
+__author__ = 'racattaque'
+
 # https://www.practicepython.org/exercise/2014/01/29/01-character-input.html:
 # Create a program that asks the user to enter their name and their age. Print out a message addressed to them that tells them the year that they will turn 100 years old.
 
