aaa42596d7c51191be1b9949eb71c1bcd85e3e7a
racattaque <50136976+racattaque@users.noreply.github.com>
2019-04-30 11:53:20 +0200

    Update Exe2.py

--- Exe2.py
+++ Exe2.py
@@ -1,3 +1,5 @@
+__author__ = 'racattaque'
+
 # https://www.practicepython.org/exercise/2014/02/05/02-odd-or-even.html:
 
 # Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user. Hint: how does an even / odd number react differently when divided by 2?
