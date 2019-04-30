aff3365c2014cca15716380331ee11a2ea907d50
racattaque <50136976+racattaque@users.noreply.github.com>
2019-04-30 11:54:17 +0200

    Update Exe6.py

--- Exe6.py
+++ Exe6.py
@@ -1,3 +1,5 @@
+__author__ = 'racattaque'
+
 # https://www.practicepython.org/exercise/2014/03/12/06-string-lists.html
 # Ask the user for a string and print out whether this string is a palindrome or not. (A palindrome is a string that reads the same forwards and backwards.)
 
