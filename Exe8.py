0cc40a265a28eadb23e432ed44af4daaa1d2bc2c
Rachid Ben <50136976+racattaque@users.noreply.github.com>
2019-04-30 14:55:28 +0200

    Update Exe8.py

--- Exe8.py
+++ Exe8.py
@@ -1,3 +1,5 @@
+__author__ = 'racattaque'
+
 # https://www.practicepython.org/exercise/2014/03/26/08-rock-paper-scissors.html:
 # Make a two-player Rock-Paper-Scissors game. (Hint: Ask for player plays (using input), compare them, print out a message of congratulations to the winner, and ask if the players want to start a new game)
 
